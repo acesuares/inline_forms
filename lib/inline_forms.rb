707e2a4fb35a2c84d16b22f638e5de5f18681ee0
Ace Suares <ace@suares.an>
2011-01-27 14:35:08 -0400

    Learning

---
+++ lib/inline_forms.rb
@@ -0,0 +1,20 @@
+module InlineForms
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def find_by_tags()
+      raise 'hello'
+    end
+  end
+
+  module InstanceMethods
+    def snier()
+      raise 'mellow'
+    end
+  end 
+end
+
+class ActiveRecord::Base
+  include InlineForms
+end
+                                           
