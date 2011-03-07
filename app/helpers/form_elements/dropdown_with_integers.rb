ced4f9e9b8722c86a761c82f902fd95b60c2cc29
ace <ace@matey.(none)>
2011-03-07 19:07:32 -0400

    Version bump to 0.7.2

---
+++ app/helpers/form_elements/dropdown_with_integers.rb
@@ -0,0 +1,26 @@
+module InlineFormsHelper
+    InlineForms::SPECIAL_MIGRATION_TYPES[:dropdown_with_integers]=:integer
+  # dropdown_with_integers generates a dropdown menu
+  # with the given list of integers as options
+  #
+  # values must be a Range or a one-dimensional array of Integers
+  def dropdown_with_integers_show(object, attribute, values)
+    unless values.is_a?(Hash)
+      options = Array.new
+      values.to_a.each_index do |i|
+        options << [ i.to_s, values.to_a[i] ]
+      end
+      values = Hash[ *options.flatten ]
+    end
+    link_to_inline_edit object, attribute, values[object.send(attribute).to_s], values
+  end
+  def dropdown_with_integers_edit(object, attribute, values)
+    # the leading underscore is to avoid name conflicts, like 'email' and 'email_type' will result in 'email' and 'email[email_type_id]' in the form!
+    values = values.sort {|a,b| a[1].to_i<=>b[1].to_i}
+    collection_select( ('_' + object.class.to_s.downcase).to_sym, attribute.to_sym, values, 'first', 'last', :selected => object.send(attribute))
+  end
+  def dropdown_with_integers_update(object, attribute, values)
+    object[attribute.to_sym] = params[('_' + object.class.to_s.downcase).to_sym][attribute.to_sym]
+  end
+end
+
--- app/models/inline_form.rb
+++
@@ -1,2 +0,0 @@
-class InlineForm < ActiveRecord::Base
-end
