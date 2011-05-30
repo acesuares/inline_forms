4aa59a61a1252b0871e39ef3b4930a99c724bc44
Ace Suares <ace@suares.an>
2011-05-30 16:43:30 -0400

    * ckeditor integration for text_area.rb

--- app/helpers/form_elements/text_area.rb
+++ app/helpers/form_elements/text_area.rb
@@ -1,22 +1,42 @@
 InlineForms::SPECIAL_COLUMN_TYPES[:text_area]=:text
 
 def text_area_show(object, attribute)
-  link_to_inline_edit object,
-                      attribute,
-                      ckeditor_textarea(  object.class,
-                                          attribute,
-                                          :width => '100%',
-                                          :height => '200px',
-                                          :value => object[attribute],
-                                          :toolbar => "None",
-                                          :resize_enabled => "false",
-                                          :toolbarCanCollapse => false ) +
-                      raw('<img class="window_pane" src="/images/clear.gif" />')
+  if defined? Ckeditor
+    link_to_inline_edit object,
+      attribute,
+      ckeditor_textarea(
+        object.class,
+        attribute,
+        :value => object[attribute],
+        :id => "textarea_#{object.class.name.underscore}_#{object.id}_#{attribute.to_s}",
+        :width => '100%',
+        :ckeditor_options => {
+          :height => '200px',
+          :toolbar => "None",
+          :readOnly => "true",
+          :resize_enabled => "false",
+          :toolbarCanCollapse => "false"
+        }
+      )
+  else
+    link_to_inline_edit object, attribute, object[attribute]
+  end
 end
 
 def text_area_edit(object, attribute)
-#  text_area_tag attribute, object[attribute], :class => 'attribute_text_area'
-  ckeditor_textarea(object.class, attribute, :width => '100%', :height => '200px', :value => object[attribute], :name => attribute )
+  if defined? Ckeditor
+    ckeditor_textarea(
+      object.class,
+      attribute,
+      :value => object[attribute],
+      :id => "textarea_#{object.class.name.underscore}_#{object.id}_#{attribute.to_s}",
+      :width => '100%',
+      :height => '200px',
+      :name => attribute
+    )
+  else
+    text_area_tag attribute, object[attribute], :class => 'attribute_text_area'
+  end
 end
 
 def text_area_update(object, attribute)
