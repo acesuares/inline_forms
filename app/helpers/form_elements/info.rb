57fa12750faed22283119f31e1decb2416ebf126
Ace Suares <ace@suares.an>
2011-07-17 19:25:57 -0400

    * other dat format (dd-mm-yyyy for real now)  * info form element, to show stuff like :id without ever updating.

---
+++ app/helpers/form_elements/info.rb
@@ -0,0 +1,13 @@
+# not needed here, since this is only used in the views InlineForms::SPECIAL_COLUMN_TYPES[:info]=:string
+
+def info_show(object, attribute)
+  object.send(attribute)
+end
+
+def info_edit(object, attribute)
+  object[attribute]
+end
+
+def info_update(object, attribute)
+end
+
