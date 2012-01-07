6ff7b591dbb5371bfd6280c7822a39bcbe55d437
Ace Suares <ace@suares.an>
2012-01-06 21:52:41 -0400

    test3 uniqueness validator

--- lib/app/validators/is_unique_validator.rb
+++ lib/app/validators/is_unique_validator.rb
@@ -1,9 +1,12 @@
-class IsUniqueValidator < ActiveRecord::Validations::UniquenessValidator
+class IsUniqueValidator  < ActiveModel::EachValidator
+  delegate :new, :setup, :validate_each, :to => ActiveRecord::Validations::UniquenessValidator
+
   def error_message
-    "is not unique."
+    "is not a unique."
   end
 
   def help_message
     "Needs to be unique."
   end
+
 end
