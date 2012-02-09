e1bcbd6fd6ab8957bd45cc8f1e47660036b58fbc
Ace Suares <ace@suares.an>
2012-02-09 11:39:28 -0400

    sliderfun

--- lib/app/helpers/form_elements/slider_with_values.rb
+++ lib/app/helpers/form_elements/slider_with_values.rb
@@ -3,20 +3,60 @@ InlineForms::SPECIAL_COLUMN_TYPES[:slider_with_values]=:integer
 # slider_with_values
 def slider_with_values_show(object, attribute)
   values = attribute_values(object, attribute)
-  link_to_inline_edit object, attribute, values[object.send(attribute)][1]
+  value = object.send(attribute)
+  display_value = values[value][1]
+  css_id = "#{object.class.to_s.underscore}_#{object.id}_#{attribute}"
+  if value == 0
+    out = "?"
+  else
+    out = "".html_safe
+    out << "<div class='slider slider_#{attribute.to_s}' id='slider_#{css_id}'></div>".html_safe
+    out << "<div class='slider_value' id='value_#{css_id}'>#{display_value}</div>".html_safe
+    out << "<div style='clear: both' />".html_safe
+    out << "<input type='hidden' name='_#{object.class.to_s.underscore}[#{attribute}]' value='0' id='input_#{css_id}' />".html_safe
+    out << ('<script>
+              $(function() {
+                var displayvalues = ' + values.collect {|x| x[1]}.inspect + ';
+                $( "#slider_' + css_id + '" ).slider(
+                  {
+                    value:' + object.send(attribute).to_s + ',
+                    range: "min",
+                    disabled: true,
+                    min: 0,
+                    max: 5,
+                    step: 1,
+                  }
+                );
+              });
+             </script>').html_safe
+  end
+  link_to_inline_edit object, attribute, out
 end
 
 def slider_with_values_edit(object, attribute)
   # the leading underscore is to avoid name conflicts, like 'email' and 'email_type' will result in 'email' and 'email[email_type_id]' in the form!
   values = attribute_values(object, attribute)
   css_id = "#{object.class.to_s.underscore}_#{object.id}_#{attribute}"
-  out = "<div id='value_#{css_id}'>#{values.assoc(object.send(attribute))[1]}</div>".html_safe
-  out << "<div id='slider_#{css_id}'></div>".html_safe
+  out = "".html_safe
+  out << "<div class='slider slider_#{attribute.to_s}' id='slider_#{css_id}'></div>".html_safe
+  out << "<div class='slider_value' id='value_#{css_id}'>#{values[object.send(attribute)][1]}</div>".html_safe
+  out << "<div style='clear: both' />".html_safe
   out << "<input type='hidden' name='_#{object.class.to_s.underscore}[#{attribute}]' value='0' id='input_#{css_id}' />".html_safe
   out << ('<script>
 	$(function() {
     var displayvalues = ' + values.collect {|x| x[1]}.inspect + ';
 		$( "#slider_' + css_id + '" ).slider(
+      {
+        value:' + object.send(attribute).to_s + ',
+        range: "min",
+        min: 0,
+        max: 5,
+        step: 1,
+        slide: function( event, ui ) {
+          $( "#input_' + css_id + '" ).val( ui.value );
+          $( "#value_' + css_id + '" ).html( displayvalues[ui.value] );
+        }
+  		}
     );
 		$( "#value_' + css_id + '" ).html(displayvalues[' + object.send(attribute).to_s + ']);
 		$( "#input_' + css_id + '" ).val(' + object.send(attribute).to_s + ');
