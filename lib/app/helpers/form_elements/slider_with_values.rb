e770b4d45a01f6280022f631569f1673a5a70b17
Ace Suares <ace@suares.an>
2012-02-27 14:18:50 -0400

    sliders improvement

--- lib/app/helpers/form_elements/slider_with_values.rb
+++ lib/app/helpers/form_elements/slider_with_values.rb
@@ -3,11 +3,11 @@ InlineForms::SPECIAL_COLUMN_TYPES[:slider_with_values]=:integer
 # slider_with_values
 def slider_with_values_show(object, attribute)
   values = attribute_values(object, attribute)
-  value = object.send(attribute)
-  display_value = values[value][1]
+  value = object.send(attribute).to_i         # should be an int
+  display_value = values.assoc(value)[1]      # values should be [ [ 0, value ], [ 3, value2 ] .... ] and we lookup the key, not the place in the array!
   css_id = "#{object.class.to_s.underscore}_#{object.id}_#{attribute}"
   if value == 0
-    out = "?"
+    out = "?"   # we use this as the 'unknown' value. So in the data, 0 should always be the unknown value. This gives problems with sliders where the real value is 0.
   else
     out = "".html_safe
     out << "<div class='slider slider_#{attribute.to_s}' id='slider_#{css_id}'></div>".html_safe
@@ -19,7 +19,7 @@ def slider_with_values_show(object, attribute)
                 var displayvalues = ' + values.collect {|x| x[1]}.inspect + ';
                 $( "#slider_' + css_id + '" ).slider(
                   {
-                    value:' + object.send(attribute).to_s + ',
+                    value:' + value.to_s + ',
                     range: "min",
                     disabled: true,
                     min: 0,
@@ -36,10 +36,12 @@ end
 def slider_with_values_edit(object, attribute)
   # the leading underscore is to avoid name conflicts, like 'email' and 'email_type' will result in 'email' and 'email[email_type_id]' in the form!
   values = attribute_values(object, attribute)
+  value = object.send(attribute).to_i         # should be an int, will be 0 if nil
   css_id = "#{object.class.to_s.underscore}_#{object.id}_#{attribute}"
+  display_value = values.assoc(value)[1]      # values should be [ [ 0, value ], [ 3, value2 ] .... ] and we lookup the key, not the place in the array!
   out = "".html_safe
   out << "<div class='slider slider_#{attribute.to_s}' id='slider_#{css_id}'></div>".html_safe
-  out << "<div class='slider_value' id='value_#{css_id}'>#{values[object.send(attribute)][1]}</div>".html_safe
+  out << "<div class='slider_value' id='value_#{css_id}'>#{display_value}</div>".html_safe
   out << "<div style='clear: both' />".html_safe
   out << "<input type='hidden' name='_#{object.class.to_s.underscore}[#{attribute}]' value='0' id='input_#{css_id}' />".html_safe
   out << ('<script>
@@ -47,7 +49,7 @@ def slider_with_values_edit(object, attribute)
     var displayvalues = ' + values.collect {|x| x[1]}.inspect + ';
 		$( "#slider_' + css_id + '" ).slider(
       {
-        value:' + object.send(attribute).to_s + ',
+        value:' + value.to_s + ',
         range: "min",
         min: 0,
         max: 5,
@@ -57,13 +59,14 @@ def slider_with_values_edit(object, attribute)
           $( "#value_' + css_id + '" ).html( displayvalues[ui.value] );
         }
   		}
-    );
-		$( "#value_' + css_id + '" ).html(displayvalues[' + object.send(attribute).to_s + ']);
-		$( "#input_' + css_id + '" ).val(' + object.send(attribute).to_s + ');
-	});
+    );').html_safe
+  out << ('$( "#value_' + css_id + '" ).html(displayvalues[' + value.to_s + ']);').html_safe
+	out << ('$( "#input_' + css_id + '" ).val(' + value.to_s + ');').html_safe
+	out << ('});
 	</script>').html_safe
   out
 end
+
 def slider_with_values_update(object, attribute)
   object[attribute.to_sym] = params[('_' + object.class.to_s.underscore).to_sym][attribute.to_sym]
 end
