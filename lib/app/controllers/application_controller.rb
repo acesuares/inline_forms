8abb3a0968885ec479b194fac4835f01a5ce7770
Ace Suares <ace@suares.an>
2012-03-09 15:46:32 -0400

    added application controller

---
+++ lib/app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+class ApplicationController < ActionController::Base
+  before_filter :set_locale
+
+  def set_locale
+    I18n.locale = extract_locale_from_subdomain || I18n.default_locale
+  end
+
+  # Get locale code from request subdomain (like http://it.application.local:3000)
+  def extract_locale_from_subdomain
+    parsed_locale = request.subdomains.first
+    I18n.available_locales.include?(parsed_locale.to_sym) ? parsed_locale : nil
+  end
+
+end
