2b273ec0270285cbf4cf89ff83f5b3f54d312eb4
Ace Suares <ace@suares.an>
2012-03-12 18:49:53 -0400

    locales in gem

--- lib/app/controllers/inline_forms_application_controller.rb
+++ lib/app/controllers/inline_forms_application_controller.rb
@@ -1,9 +1,12 @@
 class InlineFormsApplicationController < ActionController::Base
   protect_from_forgery
   layout 'devise' if :devise_controller?
+  
+  # limit available locales by setting this. Override in applicaton_controller.
+  I18n.available_locales = [ :en, :nl, :pp ]
 
   def set_locale
-    I18n.locale = extract_locale_from_subdomain || I18n.default_locale
+    I18n.locale = extract_locale_from_subdomain.to_sym || I18n.default_locale
   end
 
   # Get locale code from request subdomain (like http://it.application.local:3000)
