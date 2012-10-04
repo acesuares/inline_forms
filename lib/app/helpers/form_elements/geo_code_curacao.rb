e8b4319ccdd922fcbe28fcfbed44bb1ac139cae5
Ace Suares <ace@suares.an>
2012-10-04 11:01:06 -0400

    geocodecuracao bugs

--- lib/app/helpers/form_elements/geo_code_curacao.rb
+++ lib/app/helpers/form_elements/geo_code_curacao.rb
@@ -3,7 +3,7 @@ InlineForms::SPECIAL_COLUMN_TYPES[:geo_code_curacao]=:string
 
 # geo_code_curacao
 def geo_code_curacao_show(object, attribute)
-  attribute_value = object.send(attribute).presentation rescue nil
+  attribute_value = GeoCodeCuracao.new(object.send(attribute)).presentation rescue nil
   link_to_inline_edit object, attribute, attribute_value
 end
 def geo_code_curacao_edit(object, attribute)
@@ -19,7 +19,7 @@ end
 
 def geo_code_curacao_update(object, attribute)
   # extract the geocode
-  geo_code = params[attribute].scan(/\d\d\d\d\d\d/).to_s || nil
+  geo_code = params[attribute].scan(/\d\d\d\d\d\d/).first || nil
   object[attribute.to_sym] = GeoCodeCuracao.new(geo_code).valid? ? geo_code : nil
 end
 
