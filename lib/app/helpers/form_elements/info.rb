9c490f972b06aa41a24e9f8bb8d7bdbc9b72790f
Ace Suares <ace@suares.an>
2013-01-02 15:33:28 -0400

    distance_of_time_in_words_to_now_with_future

--- lib/app/helpers/form_elements/info.rb
+++ lib/app/helpers/form_elements/info.rb
@@ -4,19 +4,33 @@
 def info_show(object, attribute)
   # show the attribute. if it's a date/time, make it nicer. If it has a _presentation, show that instead
   o = object.send(attribute)
-  o = o.to_s + " (" + distance_of_time_in_words_to_now(o) + " ago )" if o.is_a?(Time)
+  o = o.to_s + " (" + distance_of_time_in_words_to_now(o) + ")" if o.is_a?(Time)
   o = o._presentation if o.respond_to?(:_presentation)
   o
 end
 
 def info_edit(object, attribute)
   o = object.send(attribute)
-  o = o.to_s + " (" + distance_of_time_in_words_to_now(o) + " ago )" if o.is_a?(Time)
+  o = o.to_s + " (" + distance_of_time_in_words_to_now(o) + ")" if o.is_a?(Time)
   o = o._presentation if o.respond_to?(:_presentation)
   o
 end
 
 def info_update(object, attribute)
   # do absolutely nothing
+end
+
+module ActionView::Helpers::DateHelper
+
+  def distance_of_time_in_words_to_now_with_future(from_time, include_seconds = false)
+    if from_time > Time.now()
+      'in ' + distance_of_time_in_words_to_now_without_future(from_time, include_seconds)
+    else
+      distance_of_time_in_words_to_now_without_future(from_time, include_seconds) + ' ago'
+    end
+  end
+
+  alias_method_chain :distance_of_time_in_words_to_now, :future
+
 end
 
