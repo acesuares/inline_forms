750c4b11dad5e2b1dd7fb4228c195db1714cc8e4
Ace Suares <ace@suares.com>
2013-06-12 05:51:05 -0400

    problems with jquery-ui

--- lib/inline_forms/version.rb
+++ lib/inline_forms/version.rb
@@ -1,4 +1,4 @@
 # -*- encoding : utf-8 -*-
 module InlineForms
-  VERSION = "1.6.17"
+  VERSION = "1.6.18"
 end
--- test/test_inline_forms.rb
+++
@@ -1,8 +0,0 @@
-# -*- encoding : utf-8 -*-
-require 'helper'
-
-class TestInlineForms < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
-  end
-end
