f05b24170164e5f5311b868dc3c80490cd463670
Ace Suares <ace@suares.an>
2014-02-22 15:20:41 -0400

    integer_field

---
+++ lib/app/helpers/form_elements/integer_field.rb
@@ -0,0 +1,15 @@
+# -*- encoding : utf-8 -*-
+InlineForms::SPECIAL_COLUMN_TYPES[:integer_field]=:integer
+
+def integer_field_show(object, attribute)
+  link_to_inline_edit object, attribute, object[attribute].nil? ? "<i class='fi-plus'></i>".html_safe : object[attribute]
+end
+
+def integer_field_edit(object, attribute)
+  integer_field_tag attribute, (object.send attribute.to_sym), :class => 'input_integer_field'  # for abide: , :required => true
+end
+
+def integer_field_update(object, attribute)
+  object.send :write_attribute, attribute.to_sym, params[attribute.to_sym]
+end
+
