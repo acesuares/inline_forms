55e0b7a6090e3c691e89b67bd37d3597b94283fa
Ace Suares <ace@suares.an>
2015-01-21 05:57:19 -0400

    date helper now converts to date

--- lib/app/helpers/form_elements/date.rb
+++ lib/app/helpers/form_elements/date.rb
@@ -3,12 +3,12 @@ InlineForms::SPECIAL_COLUMN_TYPES[:date_select]=:date
 
 # date
 def date_select_show(object, attribute)
-  link_to_inline_edit object, attribute, object.send(attribute).nil? ? "<i class='fi-plus'></i>".html_safe : object.send(attribute).strftime("%d-%m-%Y")
+  link_to_inline_edit object, attribute, object.send(attribute).nil? ? "<i class='fi-plus'></i>".html_safe : object.send(attribute).to_date.strftime("%d-%m-%Y")
 end
 
 def date_select_edit(object, attribute)
   css_id = 'datepicker_' + object.class.to_s.underscore + '_' + object.id.to_s + '_' + attribute.to_s
-  out = text_field_tag attribute, ( object.send(attribute).nil? ? "" : object.send(attribute).strftime("%d-%m-%Y") ), :id => css_id, :class =>'datepicker'
+  out = text_field_tag attribute, ( object.send(attribute).nil? ? "" : object.send(attribute).to_date.strftime("%d-%m-%Y") ), :id => css_id, :class =>'datepicker'
   out << "<script>$('##{css_id}').datepicker();</script>".html_safe
 end
 
--- lib/inline_forms/version.rb
+++ lib/inline_forms/version.rb
@@ -1,4 +1,4 @@
 # -*- encoding : utf-8 -*-
 module InlineForms
-  VERSION = "3.0.16"
+  VERSION = "3.0.17"
 end
