776a3a86260ed3055c947a1be753de5e15faddc7
Ace Suares <ace@suares.an>
2015-04-11 11:27:25 -0400

    added helper for a project.

---
+++ lib/app/helpers/form_elements/chicas_dropdown_with_family_members.rb
@@ -0,0 +1,29 @@
+# -*- encoding : utf-8 -*-
+InlineForms::SPECIAL_COLUMN_TYPES[:dropdown]=:belongs_to
+
+# dropdown
+def chicas_dropdown_with_family_members_show(object, attribute)
+  attribute_value = object.send(attribute)._dropdown_presentation rescue  "<i class='fi-plus'></i>".html_safe
+  link_to_inline_edit object, attribute, attribute_value
+end
+
+def chicas_dropdown_with_family_members_edit(object, attribute)
+  o = object.send(attribute) # the client
+  values = o.family.clients
+  values.sort_by! &:_dropdown_presentation
+  # the leading underscore is to avoid name conflicts, like 'email' and 'email_type' will result in 'email' and 'email[email_type_id]' in the form!
+  collection_select( ('_' + object.class.to_s.underscore).to_sym, attribute.to_s.foreign_key.to_sym, values, 'id', '_dropdown_presentation', :selected => object.send(attribute).id)
+end
+
+def chicas_dropdown_with_family_members_update(object, attribute)
+  foreign_key = object.class.reflect_on_association(attribute.to_sym).options[:foreign_key] || attribute.to_s.foreign_key.to_sym
+  old_path = File.dirname(object.image.path)
+  object[foreign_key] = params[('_' + object.class.to_s.underscore).to_sym][attribute.to_s.foreign_key.to_sym]
+  if object.save
+   # move to new location
+   new_path = File.join(Rails.root, "public/uploads/client/photo/#{object.client_id}")
+   system "mkdir -vp #{new_path}"
+   system "mv -v #{old_path} #{new_path}"
+  end
+end
+
--- lib/inline_forms/version.rb
+++ lib/inline_forms/version.rb
@@ -1,4 +1,4 @@
 # -*- encoding : utf-8 -*-
 module InlineForms
-  VERSION = "3.0.32"
+  VERSION = "3.0.34"
 end
