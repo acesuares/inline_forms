e8a67fae223f0fac29eb31825620551d4c662e46
Ace Suares <ace@suares.com>
2015-06-09 17:34:01 -0400

    Putting views back into the gem

--- bin/inline_forms_installer_core.rb
+++ bin/inline_forms_installer_core.rb
@@ -273,8 +273,8 @@ ROLES_MIGRATION
 
 append_to_file "db/seeds.rb", "Role.create({ id: 1, name: 'superadmin', description: 'Super Admin can access all.' }, without_protection: true)\n"
 
-say "- Copy views..."
-directory File.join(GENERATOR_PATH, 'lib/generators/views'), "app/views"
+# say "- Copy views..."
+# directory File.join(GENERATOR_PATH, 'lib/generators/views'), "app/views"
 
 say "- Copy stylesheets..."
 copy_file File.join(GENERATOR_PATH, 'lib/generators/assets/stylesheets/inline_forms.scss'), "app/assets/stylesheets/inline_forms.scss"
--- lib/inline_forms/version.rb
+++ lib/inline_forms/version.rb
@@ -1,4 +1,4 @@
 # -*- encoding : utf-8 -*-
 module InlineForms
-  VERSION = "3.0.45"
+  VERSION = "3.0.46"
 end
