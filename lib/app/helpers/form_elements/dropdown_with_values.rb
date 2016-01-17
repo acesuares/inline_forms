80d43c7a96a7da1c36b2caced413870e579b0516
Ace Suares <ace@suares.an>
2016-01-17 15:25:00 -0400

    forbidden options in dropdown with values

--- lib/app/helpers/form_elements/dropdown_with_values.rb
+++ lib/app/helpers/form_elements/dropdown_with_values.rb
@@ -6,11 +6,22 @@ def dropdown_with_values_show(object, attribute)
   values = attribute_values(object, attribute)
   link_to_inline_edit object, attribute, object.send(attribute) ? t(values.assoc(object.send(attribute))[1]) : "<i class='fi-plus'></i>".html_safe
 end
+
 def dropdown_with_values_edit(object, attribute)
   # the leading underscore is to avoid name conflicts, like 'email' and 'email_type' will result in 'email' and 'email[email_type_id]' in the form!
   values = attribute_values(object, attribute)
-  collection_select( ('_' + object.class.to_s.underscore).to_sym, attribute.to_sym, values, 'first', 'last', :selected => object.send(attribute))
+
+  attributes = @inline_forms_attribute_list || object.inline_forms_attribute_list
+  options_disabled = attributes.assoc(attribute.to_sym)[4]
+
+  collection_select(  ('_' + object.class.to_s.underscore).to_sym,
+                      attribute.to_sym,
+                      values, 'first', 'last',
+                      :selected => object.send(attribute),
+                      disabled: options_disabled,
+                    )
 end
+
 def dropdown_with_values_update(object, attribute)
   object[attribute.to_sym] = params[('_' + object.class.to_s.underscore).to_sym][attribute.to_sym]
 end
