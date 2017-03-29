eefc10a612b004a983b204dbf8afc95c15bdd955
Ace Suares <ace@suares.com>
2017-03-29 19:46:42 -0400

    text_area when empty now just a plus sign

--- lib/app/helpers/form_elements/text_area.rb
+++ lib/app/helpers/form_elements/text_area.rb
@@ -2,29 +2,33 @@
 InlineForms::SPECIAL_COLUMN_TYPES[:text_area]=:text
 
 def text_area_show(object, attribute)
-  if defined? Ckeditor
-    link_to_inline_edit object,
-      attribute,
-      '<div class="ckeditor_area">'.html_safe +
-      cktext_area_tag(
-      attribute,
-      object[attribute],
-      :id => "textarea_#{object.class.name.underscore}_#{object.id}_#{attribute.to_s}",
-      :ckeditor => {  :width => '100%',
-                      :height => '200px',
-                      :toolbar => "None",
-                      :readOnly => "true",
-                      :resize_enabled => "false",
-                      :toolbarCanCollapse => "false"
-                    }
-    ) +
-      image_tag(  'glass_plate.gif',
-      :class => "glass_plate",
-      :title => '' ) +
-      "<script>delete CKEDITOR.instances['textarea_#{object.class.name.underscore}_#{object.id}_#{attribute.to_s}']</script>".html_safe +
-      '</div>'.html_safe
+  if object.send(attribute).blank?
+    link_to_inline_edit object, attribute, "<i class='fi-plus'></i>".html_safe
   else
-    link_to_inline_edit object, attribute, object[attribute]
+    if defined? Ckeditor
+      link_to_inline_edit object,
+        attribute,
+        '<div class="ckeditor_area">'.html_safe +
+        cktext_area_tag(
+        attribute,
+        object[attribute],
+        :id => "textarea_#{object.class.name.underscore}_#{object.id}_#{attribute.to_s}",
+        :ckeditor => {  :width => '100%',
+                        :height => '200px',
+                        :toolbar => "None",
+                        :readOnly => "true",
+                        :resize_enabled => "false",
+                        :toolbarCanCollapse => "false"
+                      }
+      ) +
+        image_tag(  'glass_plate.gif',
+        :class => "glass_plate",
+        :title => '' ) +
+        "<script>delete CKEDITOR.instances['textarea_#{object.class.name.underscore}_#{object.id}_#{attribute.to_s}']</script>".html_safe +
+        '</div>'.html_safe
+    else
+      link_to_inline_edit object, attribute, object[attribute]
+    end
   end
 end
 
@@ -38,7 +42,7 @@ def text_area_edit(object, attribute)
                       :height => '200px'
                     }
     )  +
-    "<script>delete CKEDITOR.instances['textarea_#{object.class.name.underscore}_#{object.id}_#{attribute.to_s}']</script>".html_safe 
+    "<script>delete CKEDITOR.instances['textarea_#{object.class.name.underscore}_#{object.id}_#{attribute.to_s}']</script>".html_safe
   else
     text_area_tag attribute, object[attribute], :class => 'attribute_text_area'
   end
