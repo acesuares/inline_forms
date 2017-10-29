2d325081a6078d43bc44dcd93cfeb408c775a08e
Manuel Ortega <manuel@lasindias.coop>
2017-10-29 22:50:18 +0100

    Added deploy env variables to application.yml file. Added figaro load instructions to Capfile. Added references to env variables in deploy and production files

--- lib/generators/templates/deploy.rb
+++ lib/generators/templates/deploy.rb
@@ -1,5 +1,5 @@
-set :application, "YOUR_APPLICATION_NAME"
-set :repo_url, "file:///home/#{user}/git-repos/#{application}.git"
+set :application, ENV['DEPLOY_APPLICATION']
+set :repo_url, ENV['DEPLO_REPO_URL']
 set :bundle_binstubs, nil
 
 # Default deploy_to directory is /var/www/my_app_name
@@ -22,6 +22,45 @@ after "deploy:publishing", "deploy:restart"
 namespace :deploy do
   task :restart do
     invoke "unicorn:restart"
+  end
+
+  desc "Zero-downtime restart of Unicorn"
+  task :zero_restart, :except => { :no_release => true } do
+    run "kill -s USR2 `cat #{shared_path}/pids/unicorn.pid`"
+  end
+
+  desc "Downtime restart of Unicorn"
+  task :restart, :except => { :no_release => true } do
+    run "kill -s KILL `cat #{shared_path}/pids/unicorn.pid`"
+    sleep 10
+    run "rvm rvmrc trust #{current_release}"
+    run "cd #{current_path} ; bundle exec r200_unicorn -c #{current_path}/config/unicorn.rb -D -E production"
+  end
+
+
+  desc "Start unicorn"
+  task :start, :except => { :no_release => true } do
+    run "rvm rvmrc trust #{current_release}"
+    run "cd #{current_path} ; bundle exec r200_unicorn -c #{current_path}/config/unicorn.rb -D -E production"
+  end
+
+  desc "Stop unicorn"
+  task :stop, :except => { :no_release => true } do
+    run "kill -s QUIT `cat #{shared_path}/pids/unicorn.pid`"
+  end
+
+  desc "Kill unicorn"
+  task :kill, :except => { :no_release => true } do
+    run "kill -s KILL `cat #{shared_path}/pids/unicorn.pid`"
+  end
+
+  desc "Fix Stuff."
+  task :fix_stuff do
+    run "cd #{shared_path} && mkdir -p log"
+    run "cd #{shared_path} && mkdir -p sockets"
+    run "ln -s #{shared_path}/sockets #{release_path}/tmp/sockets"
+    run "cd #{shared_path} && mkdir -p uploads"
+    run "ln -s #{shared_path}/uploads #{release_path}/public/uploads"
   end
 end
 
--- lib/generators/templates/production.rb
+++ lib/generators/templates/production.rb
@@ -1,7 +1,7 @@
-set :user, "root"
+set :user, ENV["DEPLOY_USER"]
 set :deploy_via, :remote_cache
 set :conditionally_migrate, true
 set :rails_env, "production"
 
 # Server IP
-server "165.227.102.11", user: fetch(:user), port: fetch(:port), roles: %w(web app db)
+server ENV["DEPLOY_HOST"], user: fetch(:user), port: fetch(:port), roles: %w(web app db)
