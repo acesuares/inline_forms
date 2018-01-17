de9263380ef743face4f908b61277e0e18ceb309
Manuel Ortega <manuel@lasindias.coop>
2018-01-17 15:37:12 -0400

    Moved stylesheets generator into inline_forms folder because of namespaces

--- lib/generators/inline_forms/stylesheets_generator.rb
+++ lib/generators/inline_forms/stylesheets_generator.rb
@@ -1,12 +1,12 @@
 module InlineForms
   module Generators
     class StylesheetsGenerator < Rails::Generators::Base
-      source_root File.expand_path('../stylesheets', __FILE__)
+      source_root File.expand_path('../../assets', __FILE__)
 
       desc 'Update css in main app from inline_forms file'
       def update_css
         remove_file 'app/assets/stylesheeets/inline_forms.scss'
-        copy_file 'inline_forms.scss', 'app/assets/stylesheeets/inline_forms.scss'
+        copy_file 'stylesheets/inline_forms.scss', 'app/assets/stylesheeets/inline_forms.scss'
       end
     end
   end
