32bd6cb481390f71da5817d366e9713019dd840d
Manuel Ortega <manuel@lasindias.coop>
2018-01-17 14:53:53 -0400

    Added update_css generator

---
+++ lib/generators/update_css_generator.rb
@@ -0,0 +1,13 @@
+module InlineForms
+  module Generators
+    class UpdateCssGenerator < Rails::Generators::Base
+      source_root File.expand_path('../stylesheets', __FILE__)
+
+      desc 'Update css in main app from inline_forms file'
+      def update_css
+        remove_file 'app/assets/stylesheeets/inline_forms.scss'
+        copy_file 'inline_forms.scss', 'app/assets/stylesheeets/inline_forms.scss'
+      end
+    end
+  end
+end
