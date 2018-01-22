87850510b3356d5f25e177a14978e37cfeda1bdb
Manuel Ortega <manuel@lasindias.coop>
2018-01-22 15:25:21 -0400

    From this inline_forms version, css style are loaded directly from the gem

--- lib/inline_forms.rb
+++ lib/inline_forms.rb
@@ -152,6 +152,10 @@ module InlineForms
       end
     end
 
+    initializer "inline_forms.assets.precompile" do |app|
+      app.config.assets.precompile += %w( inline_forms/inline_forms.scss )
+    end
+
     I18n.load_path << Dir[File.join(File.expand_path(File.dirname(__FILE__) + '/locales'), '*.yml')]
     I18n.load_path.flatten!
   end
