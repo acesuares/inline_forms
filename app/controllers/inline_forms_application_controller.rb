891c1dcefe0a3289e3fe9601ca591e67e0651aef
Ace Suares <ace@suares.com>
2020-07-01 12:34:06 -0400

    problem with cancan_disabled? temp solution

--- app/controllers/inline_forms_application_controller.rb
+++ app/controllers/inline_forms_application_controller.rb
@@ -2,7 +2,7 @@
 class InlineFormsApplicationController < ActionController::Base
   protect_from_forgery
   layout 'devise' if :devise_controller?
-  
+
   # limit available locales by setting this. Override in applicaton_controller.
   I18n.available_locales = [ :en, :nl, :pp ]
 
@@ -10,7 +10,7 @@ class InlineFormsApplicationController < ActionController::Base
   def set_locale
     I18n.locale = extract_locale_from_subdomain || I18n.default_locale
   end
-  
+
   # Get locale code from request subdomain (like http://it.application.local:3000)
   def extract_locale_from_subdomain
     locale = request.subdomains.first
--- app/helpers/inline_forms_helper.rb
+++ app/helpers/inline_forms_helper.rb
@@ -130,7 +130,8 @@ module InlineFormsHelper
     spaces = attribute_value.length > 40 ? 0 : 40 - attribute_value.length
     value = h(attribute_value) + ("&nbsp;" * spaces).html_safe
     css_class_id = "#{object.class.to_s.underscore}_#{object.id}_#{attribute}"
-    if cancan_disabled? || ( can? :update, object, attribute )
+    if (cancan_disabled? rescue true) || ( can? :update, object, attribute )
+      # some problem with concerns makes this function not available when called direct. FIXME
       link_to value,
         send( 'edit_' + object.class.to_s.underscore + '_path',
         object,
