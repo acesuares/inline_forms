85b62683b6e068fa5bb561098d0dc67a91ce00a1
Ace Suares <ace@suares.an>
2024-02-07 11:25:06 -0400

    added simple file field

---
+++ app/helpers/form_elements/simple_file_field.rb
@@ -0,0 +1,28 @@
+# -*- encoding : utf-8 -*-
+InlineForms::SPECIAL_COLUMN_TYPES[:simple_file_field]=:string
+
+def simple_file_field_show(object, attribute)
+  o = object.send(attribute)
+  
+  if o.send(:present?)
+    filename = o.to_s
+    model = object.class.to_s.pluralize.underscore
+    link_to filename, "/#{model}/dl/#{object.id}" # route must exist!!
+  else
+    link_to_inline_edit object, attribute, msg
+  end
+end
+
+def simple_file_field_edit(object, attribute)
+  file_field_tag attribute, :class => 'input_text_field'
+end
+
+def simple_file_field_update(object, attribute)
+  object.send(attribute.to_s + '=', params[attribute.to_sym])
+end
+
+# You need to add a route to your routes.rb file: 
+# get '/:model/dl/:id' => 'your_controller#download', :as => 'download'
+# and a method to your controller:
+# def download
+# FIXME
